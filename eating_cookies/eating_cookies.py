d8ff4b1674c0a1cc30e635382b6085ec27e663d6
glassbones <glassbonesofficial@gmail.com>
2020-08-20 15:00:20 -0500

    slow

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,12 @@
 Input: an integer
 Returns: an integer
 '''
+# 0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    trib=[1,1,2]
+    if( n <= 2 ): return trib[n]
+    for x in range(n-2): trib.append( trib[-1] + trib[-2] + trib[-3] )
+    return trib[-1]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python
 
 import sys
+import math
+
+
+def change(n):
+  if n != 3:return n + 1
+  else: return 1
+
+def makeList(n):
+  l = []
+  while (n > 0):
+    l.append(change(n))
+    n-= 1
+  return l
 
 def rock_paper_scissors(n):
-  # Your code here
+  lists = {}
+  l = []
+  power = (3 ** n)
+  while(power > 0):
+    print(f'{power}')
+    lists.update( {f'l{power}' : makeList(n)} )
+    power -= 1
 
-  pass
+  return str(lists.values())[12:-1]
 
+
+print(rock_paper_scissors(3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
