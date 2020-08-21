3c06f5efa478a92cd6e14818ea3beb22525dcbbb
glassbones <glassbonesofficial@gmail.com>
2020-08-21 01:31:00 -0500

    added rock paper scizzors semi solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,31 +3,31 @@
 import sys
 import math
 
+def dig(lists, n, value):
+  if len(lists) == n: return lists.insert(0, value)
+  for l in lists: dig(l, n, value)
 
-def change(n):
-  if n != 3:return n + 1
-  else: return 1
-
-def makeList(n):
-  l = []
-  while (n > 0):
-    l.append(change(n))
-    n-= 1
-  return l
-
-def rock_paper_scissors(n):
-  lists = {}
-  l = []
-  power = (3 ** n)
-  while(power > 0):
-    print(f'{power}')
-    lists.update( {f'l{power}' : makeList(n)} )
-    power -= 1
-
-  return str(lists.values())[12:-1]
+def rpc(n):
+  if n == 1: return [0],[1],[2]
+  lists = [rpc(n-1) for i in range(3)]
+  for insertValue, item in enumerate(lists):
+    dig(item, n-1, insertValue)
+  return lists
 
 
-print(rock_paper_scissors(3))
+print('')
+print('')
+print('input = 1')
+print(rpc(1))
+print('')
+print('')
+print('input = 2')
+print(rpc(2))
+print('')
+print('')
+print('input = 3')
+print(rpc(3))
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
